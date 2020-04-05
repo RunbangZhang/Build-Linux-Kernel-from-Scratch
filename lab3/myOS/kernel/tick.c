32ef7d024ca3292241c2b36e01b578f581d78f22
214894 <sqm1226@gmail.com>
2020-04-05 23:58:24 +0800

    add to /src

--- lab3/myOS/kernel/tick.c
+++
@@ -1,9 +0,0 @@
-extern void maybeUpdateWallClock(void);
-int count=0;	//维护tick
-
-void tick(void)			//处理周期性时钟中断
-{
-	// TODO 请填写代码
-	maybeUpdateWallClock();
-	++count;
-}
--- lab3/userApp/main.c
+++
@@ -1,6 +0,0 @@
-extern void startShell(void);
-void myMain(void)
-{
-	// TODO 请填写代码
-	startShell();
-}
