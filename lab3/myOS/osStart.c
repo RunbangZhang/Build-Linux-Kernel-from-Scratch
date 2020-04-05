2feade2a164f2b71c6e19e8e704c9d50540273c3
214894 <sqm1226@gmail.com>
2020-04-05 16:04:04 +0800

    add init module in osStart.c

--- lab3/myOS/osStart.c
+++ lab3/myOS/osStart.c
@@ -1,14 +1,21 @@
 extern int myPrintk(int color,const char *format, ...);
 extern void clear_screen(void);
 extern void myMain(void);
+extern void init8253(void);
+extern void init8259A(void);
+extern void enable_interrupt(void);
+extern void disable_interrupt(void);
 
 void osStart(void)
 {
 	// TODO 请填写代码
-
+	init8259A();
+	init8253();
+	enable_interrupt();
+	
 	clear_screen();
-	myPrintk(0x2,"START RUNNING......\n");
+	//myPrintk(0x2,"START RUNNING......\n");
 	myMain();
-	myPrintk(0x2, "STOP RUNNING......ShutDown\n");
+	//myPrintk(0x2, "STOP RUNNING......ShutDown\n");
 	while(1);
 }
