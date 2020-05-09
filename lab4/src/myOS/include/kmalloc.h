c9e8b525ef3e133d47e2626d8a224963decc535a
214894 <sqm1226@gmail.com>
2020-05-09 20:33:28 +0800

    malloc.h & kmalloc.h

---
+++ lab4/src/myOS/include/kmalloc.h
@@ -0,0 +1,13 @@
+#ifndef __KMALLOC_H__
+#define __KMALLOC_H__
+
+#include "mem.h"
+
+//需要实现！！！（可选）
+	/*
+	这里实现kmalloc/kfree，调用dp或者是efp里的函数实现即可
+	*/
+#define kmalloc(size)  dPartitionAlloc(pMemHandler,size)
+#define kfree(start)		dPartitionFree(pMemHandler,start)
+
+#endif
