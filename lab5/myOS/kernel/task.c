2b0da8ebdddb7fe294999c4c12b992f272e0154c
214894 <sqm1226@gmail.com>
2020-06-05 17:12:39 +0800

    [task.c] 实现任务所有接口

--- lab5/myOS/kernel/task.c
+++ lab5/myOS/kernel/task.c
@@ -16,37 +16,74 @@ typedef struct rdyQueueFCFS{
 
 rdyQueueFCFS rqFCFS;
 
-void rqFCFSInit(myTCB* idleTsk) {
+void rqFCFSInit(myTCB * idleTsk) {
+	rqFCFS.head = (void*)0;
+	rqFCFS.tail = (void*)0;
+	rqFCFS.idleTsk = idleTsk;
 }
 
 int rqFCFSIsEmpty(void) {
+	return ((rqFCFS.head == (void*)0)&&(rqFCFS.tail == (void*)0));
 }
 
 myTCB * nextFCFSTsk(void) {
+	if(rqFCFSIsEmpty())
+		return rqFCFS.idleTsk;
+	else return rqFCFS.head;
 }
 
 /* tskEnqueueFCFS: insert into the tail node */
 void tskEnqueueFCFS(myTCB *tsk) {
+
+     if(rqFCFSIsEmpty())
+          rqFCFS.head = tsk;
+     else
+          rqFCFS.tail->next = tsk;
+
+     rqFCFS.tail = tsk;
 }
 
 /* tskDequeueFCFS: delete the first node */
-void tskDequeueFCFS(myTCB *tsk) {
+void tskDequeueFCFS(myTCB * tsk) {
+	rqFCFS.head = rqFCFS.head->next;
+
+	if(tsk == rqFCFS.tail)
+		rqFCFS.tail = (void*)0;
 }
+
+
 
 // 用于初始化新创建的 task 的栈
 // 这样切换到该任务时不会 stack underflow
 void stack_init(unsigned long **stk, void (*task)(void)){
+	*(*stk)-- = (unsigned long) 0x08;
+    *(*stk)-- = (unsigned long) task;
+    *(*stk)-- = (unsigned long) 0x0202;
+
+    *(*stk)-- = (unsigned long) 0xAAAAAAAA;
+    *(*stk)-- = (unsigned long) 0xCCCCCCCC;
+    *(*stk)-- = (unsigned long) 0xDDDDDDDD;
+    *(*stk)-- = (unsigned long) 0xBBBBBBBB;
+
+    *(*stk)-- = (unsigned long) 0x44444444;
+    *(*stk)-- = (unsigned long) 0x55555555;
+    *(*stk)-- = (unsigned long) 0x66666666;
+    *(*stk)   = (unsigned long) 0x77777777;    
 }
 
 /**
  * 必须实现的外部接口
  */
 void tskStart(myTCB *tsk){
-#error "TODO: 实现本函数"
+//#error "TODO: 实现本函数"
+	tsk->state = TSK_RDY;
+	tskEnqueueFCFS(tsk);
 }
 
 void tskEnd(void){
-#error "TODO: 实现本函数"
+//#error "TODO: 实现本函数"
+	tskDequeueFCFS(currentTsk);
+	destroyTsk(currentTsk->tcbIndex);
 }
 
 /* createTsk
@@ -54,7 +91,18 @@ void tskEnd(void){
  * return value: taskIndex or, if failed, -1
  */
 int createTsk(void (*tskBody)(void)) {
-#error "TODO: 实现本函数"
+//#error "TODO: 实现本函数"
+
+	myTCB * allocated = firstFreeTsk;
+	if(!allocated) return -1;
+
+	allocated->state = 0;
+	stack_init(&(allocated->stkTop), tskBody);
+	tskStart(allocated);
+
+	firstFreeTsk = allocated->next;
+
+	return allocated->tcbIndex;
 }
 
 /* destroyTsk
@@ -62,17 +110,28 @@ int createTsk(void (*tskBody)(void)) {
  * return value: void
  */
 void destroyTsk(int takIndex) {
-#error "TODO: 实现本函数"
+//#error "TODO: 实现本函数"
+	tcbPool[takIndex].state = -1;
+
+	schedule();
 }
 
 unsigned long **prevTSK_StackPtr;
 unsigned long *nextTSK_StackPtr;
 void context_switch(myTCB *prevTsk, myTCB *nextTsk) {
-#error "TODO: 实现本函数"
+//#error "TODO: 实现本函数"
+	prevTSK_StackPtr = &prevTsk->stkTop;	
+	nextTSK_StackPtr = nextTsk->stkTop;
+
+	CTX_SW(prevTSK_StackPtr,nextTSK_StackPtr);
 }
 
 void scheduleFCFS(void) {
-#error "TODO: 实现本函数"
+//#error "TODO: 实现本函数"
+	myTCB * prevTsk = currentTsk;
+	currentTsk = nextFCFSTsk();
+
+	context_switch(prevTsk,currentTsk);
 }
 
 void schedule(void) {
@@ -83,7 +142,9 @@ void schedule(void) {
  * idle 任务
  */
 void tskIdleBdy(void) {
-#error "TODO: 实现本函数"
+//#error "TODO: 实现本函数"
+	while(rqFCFSIsEmpty);
+	schedule();
 }
 
 unsigned long BspContextBase[STACK_SIZE];
@@ -99,9 +160,55 @@ void startMultitask(void) {
 }
 
 void TaskManagerInit(void) {
-#error "TODO: 实现本函数"
+//#error "TODO: 实现本函数"
      // 初始化 TCB 数组
      // 创建 idle 任务
      // 创建 init 任务（使用 initTskBody）
      // 切入多任务状态
+     int i;
+     myTCB *thisTCB;
+
+     for(i=0;i<TASK_NUM;i++){
+          thisTCB=&tcbPool[i];
+
+          //init index
+          thisTCB->tcbIndex=i;
+
+          //init freelist
+          if(i==TASK_NUM-1)
+               thisTCB->next=(myTCB *)0;
+          else 
+               thisTCB->next=&tcbPool[i+1];
+
+          //init stkTop 
+          thisTCB->stkTop = thisTCB->stack + STACK_SIZE-1;
+     }
+
+     //task0:Idle create and start
+     idleTsk = &tcbPool[0];
+     stack_init(&(idleTsk->stkTop),tskIdleBdy);
+     rqFCFSInit(idleTsk);
+
+     firstFreeTsk = &tcbPool[1];
+
+     //task0:myMain
+     createTsk(initTskBody);
+
+     myPrintk(0x2,"START MULTITASKING......\n");
+     startMultitask();
+     myPrintk(0x2,"STOP MULTITASKING......ShutDown\n");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
