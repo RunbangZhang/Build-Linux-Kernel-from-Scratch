298bc5b5b08462ce9abf3e15e89f054718f7a51e
214894 <sqm1226@gmail.com>
2020-06-06 09:38:41 +0800

    [main.c] 封装shell 创建task

--- lab5/userApp/main.c
+++ lab5/userApp/main.c
@@ -1,6 +1,21 @@
 #include "../myOS/userInterface.h"   //interface from kernel
 
 #include "shell.h"
+#include "memTestCase.h"
+
+void wallClock_hook_main(void){
+     int _h, _m, _s;
+     char hhmmss[]="hh:mm:ss\0\0\0\0";
+
+     getWallClock(&_h,&_m,&_s);
+     sprintf(hhmmss,"%02d:%02d:%02d",_h,_m,_s);
+     put_chars(hhmmss,0x7E,24,72);
+}
+
+void doSomeTestBefore(void){       
+     setWallClock(18,59,59);       //set time 18:59:59
+     setWallClockHook(&wallClock_hook_main);
+}
 
 void myTsk0(void);
 void myTsk1(void);
@@ -11,11 +26,19 @@ void startShell(void);
 void myMain(void) {
      clear_screen();
 
+     doSomeTestBefore();
+
+     myPrintf(0x7, "********************************\n");
+	 myPrintf(0x7, "*        INIT   INIT !         *\n");
+	 myPrintf(0x7, "********************************\n");
+
      createTsk(myTsk0);
      createTsk(myTsk1);
      createTsk(myTsk2);
 
-#error "TODO: 初始化 shell 并创建 shell task"
+     initShell();
+     memTestCaseInit();    //应该可不加
+     createTsk(startShell);
 
      tskEnd();
 }
