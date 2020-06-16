5aead01204bc7450eb233d00fae0e99f00204632
214894 <sqm1226@gmail.com>
2020-06-16 21:41:19 +0800

    [task_prio0.c] 添增注释

--- lab6/src/myOS/kernel/task_sched/task_prio0.c
+++ lab6/src/myOS/kernel/task_sched/task_prio0.c
@@ -1,33 +1,32 @@
 #include "../../include/task_sched.h"
 #include "../../include/taskPara.h"
 
-myTCB *rqPRIO0;  //ready queue
+myTCB *rqPRIO0;  //优先级调度队列
 
-void rqPRIO0Init(void) {
+void rqPRIO0Init(void) {		//初始化优先级调度队列
 	rqPRIO0 = idleTsk;	// head <- idleTsk
 	dLinkListInit((dLinkedList *)(&(rqPRIO0->thisNode)));
 }
 
-myTCB * nextPRIO0Tsk(void) {
+myTCB * nextPRIO0Tsk(void) {	//返回队列中下一个待调度的任务
 	return (myTCB*)dLinkListFirstNode((dLinkedList*)rqPRIO0);	
 }
 
 
-void tskEnqueuePRIO0(myTCB *tsk){
-	//dLinkInsertBefore((dLink_node *)rqPRIO0,(dLink_node *)rqPRIO0,(dLink_node*)tsk);
+void tskEnqueuePRIO0(myTCB *tsk){ 			//任务入队列
     dLink_node *q = ((dLinkedList*)rqPRIO0)->next;
-    while(q!=(dLinkedList*)rqPRIO0 && ((myTCB *)q)->para.priority<tsk->para.priority)
+    while(q!=(dLinkedList*)rqPRIO0 && ((myTCB *)q)->para.priority<tsk->para.priority)	//按优先级从小到大顺序入队
          q=q->next;
     dLinkInsertBefore((dLinkedList*)rqPRIO0,q,(dLink_node *)tsk);
 }
 
 
-void tskDequeuePRIO0(myTCB *tsk){
+void tskDequeuePRIO0(myTCB *tsk){			//任务出队列
 	dLinkDelete((dLinkedList*)rqPRIO0,(dLink_node*)tsk);	
 }
 
 
-void schedulerInit_PRIO0(void){
+void schedulerInit_PRIO0(void){            //调度器初始化
     rqPRIO0Init();
 
 	/* default for all task except idleTsk*/
@@ -37,10 +36,10 @@ void schedulerInit_PRIO0(void){
 	_setTskPara(idleTsk,&defaultTskPara);
 	idleTsk->para.schedPolicy = SCHED_IDLE;
 
-	initTsk_para.priority = 0;
+	initTsk_para.priority = 0;			  //init任务优先级设为0，为最高优先级
 }
 
-struct scheduler scheduler_PRIO0 = {
+struct scheduler scheduler_PRIO0 = {	  //将各接口封装成一个调度器
     .type = SCHEDULER_PRIORITY0,
     .nextTsk_func = nextPRIO0Tsk,
     .enqueueTsk_func = tskEnqueuePRIO0,
